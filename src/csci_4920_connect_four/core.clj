c6eb74e625e3bcb2200e6ea9c585bfa6c9e15669
Andrew Hill <andrew.2.hill@ucdenver.edu>
2017-02-19 20:30:52 -0700

    Added function to apply a move (represented as a column number) to a given board state. If the column is full, function returns nil.

--- src/csci_4920_connect_four/core.clj
+++ src/csci_4920_connect_four/core.clj
@@ -5,23 +5,42 @@
 (defn receive-game-string []
   (parse-string (read-line)))
 
+(defn apply-move
+  [state move player]
+  ;; Bind column to the column selected by 'move'
+  (let [column (get state move)]
+    ;; If the first entry in the column is non-zero, then return nil because this column is full.
+    (if-not (= (first column) 0)
+      nil
+      ;; If the last element in the column is zero, immediately insert at the end.
+      (if (= (peek column) 0)
+        ;; Associate the value at [move column.size-1] with the player's number (represents their move)
+        (assoc-in state [move (- (count column) 1)] player)
+        ;; Loop with initial index binding of 1 (We already checked zero in the first if-statement.
+        (loop [index 1]
+          ;; If this value is zero, recursively call with ++index.
+          (if (= (get column index) 0)
+            ;; If true recursively call.
+            (recur (inc index))
+            ;; If false, stop here and associate the given position with the player's number (move).
+            (assoc-in state [move (- index 1)] player)
+            )
+          )
+        )
+      )
+    )
+  )
 (defn -main
   [& args]
   ;; Bind 'gamestring' to the string parsed from input.
   (let [gamestring (receive-game-string)]
     ;; Bind 'board', 'width', and 'height' to their correseponding values in the map we get from receive-game-string.
-    (let [board (get gamestring "grid") width (get gamestring "width") height (get gamestring "height")]
+    (let [board (get gamestring "grid") width (get gamestring "width") height (get gamestring "height") player (get gamestring "player")]
       ;; Print each to validate.
       (println (str "Board: " board))
       (println (str "Width: " width))
       (println (str "Height: " height))
+
       )
     )
   )
-
-
-
-
-
-
-
