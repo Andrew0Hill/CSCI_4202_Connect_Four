96adb6224a60e006c35a935b18588f0a8f5b132e
Andrew Hill <andrew.2.hill@ucdenver.edu>
2017-02-23 14:59:52 -0700

    might work, might not

--- src/csci_4202_connect_four/core.clj
+++ src/csci_4202_connect_four/core.clj
@@ -2,7 +2,10 @@
   (:gen-class)
   (:require [cheshire.core :refer :all]
             [clojure.core.matrix :refer :all])
-  (:import (java.io BufferedReader BufferedWriter)))
+  (:import (java.io BufferedReader BufferedWriter)
+           (java.lang.System)))
+
+(def move-values {4 100000 3 10000 2 100 1 0})
 (declare min-r)
 (declare max-r)
 
@@ -10,7 +13,7 @@
   (parse-string (read-line)))
 
 
-(def MAXDEPTH 3)
+(def MAXDEPTH 2)
 (defn apply-move
   [state move player]
   ;; Bind column to the column selected by 'move'
@@ -41,48 +44,67 @@
           )
         )
   )
-(defn random-move [state player]
-  (loop [states (get-valid-moves state player) num (rand-int (count states))]
-    (if-not (nil? (get states num))
-      num
-      (recur (dissoc states num) (rand-int (count states)))
+(defn switch-player [player]
+  (if (= player 1)
+    2
+    1
+    )
+  )
+(defn get-column-fours [state]
+  (apply concat
+         (map #(partition 4 1 %) state))
+  )
+(defn get-row-fours [state]
+  (apply concat
+         (map #(partition 4 1 %) (transpose state)))
+  )
+(defn get-diagonal-fours [state]
+  (let [vec [] rev (reverse state)]
+    (apply concat
+           (map #(partition 4 1 %)
+                (filter
+                  #(>= (count %) 4)
+                  (apply concat
+                         (for [x (range (- 1 (count state)) (count (first state)))]
+                           (conj vec (diagonal state x) (diagonal rev x))
+                           )
+                         )
+                  )
+                )
+           )
+    )
+  )
+(defn points-per-four [group player]
+  (let [fmap (frequencies group) opponent (switch-player player)]
+    (if (and (contains? fmap player) (not (contains? fmap opponent)))
+      (get move-values (get fmap player))
+      (if (and (contains? fmap opponent) (not (contains? fmap player)))
+        (* -1 (get move-values (get fmap opponent)))
+        0
+        )
       )
     )
   )
-(defn utility [state]
-  (rand-int 100)
+(defn utility [state player]
+  (apply + (map #(points-per-four % player) (concat (get-column-fours state) (get-row-fours state) (get-diagonal-fours state))))
   )
+
 (defn check-diag [state]
-  (let [vec [] rev (reverse state)]
-    (some true? (map #(when (not= (first %) 0) (apply = %))
-                     (apply concat
-                            (map #(partition 4 1 %)
-                                 (filter
-                                   #(>= (count %) 4)
-                                   (apply concat
-                                          (for [x (range (- 1 (count state)) (count (first state)))]
-                                            (conj vec (diagonal state x) (diagonal rev x))
-                                            )
-                                          )
-                                   )
-                                 )
-                            )
-                     )
-          )
-    )
+  (some true? (map #(when (not= (first %) 0) (apply = %))
+                   (get-diagonal-fours state)
+                   )
+        )
   )
 (defn check-column [state]
   (some true?
         (map #(when (not= (first %) 0) (apply = %))
-             (apply concat
-                    (map #(partition 4 1 %) state)))
+             (get-column-fours state))
         )
   )
 (defn check-row [state]
   (some true?
         (map #(when (not= (first %) 0) (apply = %))
-             (apply concat
-                    (map #(partition 4 1 %) (transpose state))))
+             (get-row-fours state))
         )
   )
 (defn end-game [state]
@@ -95,26 +117,31 @@
   ;; Check each row for end state
 
   )
-(defn switch-player [player]
-  (if (= player 1)
-    2
-    1
+(defn random-move [state player]
+  (when (not (end-game state))
+    (loop [states (get-valid-moves state player) num (rand-int (count states))]
+      (if-not (nil? (get states num))
+        num
+        (recur (dissoc states num) (rand-int (count states)))
+        )
+      )
     )
   )
-(defn min-r [state alpha beta depth player]
+
+(defn min-r [state alpha beta depth curr-player player]
   (if (or (nil? state) (end-game state) (= depth MAXDEPTH))
     ;; If max depth, return the utility of this state.
-    (utility state)
-    (let [state-map (get-valid-moves state player) size (count state-map)]
-      (loop [v 10000 b-val beta current 0]
+    (utility state player)
+    (let [state-map (get-valid-moves state curr-player) size (count state-map)]
+      (loop [v 100000 b-val beta current 0]
         (let [
               state (get state-map current)
-              x (min v (max-r state alpha beta (inc depth) player))
+              x (min v (max-r state alpha beta (inc depth) (switch-player curr-player) player))
               newbeta (min b-val x)
               ]
           (if (or (<= b-val alpha) (= size (- current 1)))
             (do
-              (println "pruned")
+              ;;(println "pruned")
               x)
             (recur x newbeta (inc current))
             )
@@ -123,20 +150,20 @@
       )
     )
   )
-(defn max-r [state alpha beta depth player]
+(defn max-r [state alpha beta depth curr-player player]
   (if (or (nil? state) (end-game state) (= depth MAXDEPTH))
     ;; If max depth, return the utility of this state.
-    (utility state)
-    (let [state-map (get-valid-moves state player) size (count state-map)]
-      (loop [v -10000 a-val alpha current 0]
+    (utility state player)
+    (let [state-map (get-valid-moves state curr-player) size (count state-map)]
+      (loop [v -100000 a-val alpha current 0]
         (let [
               state (get state-map current)
-              x (max v (min-r state alpha beta (inc depth) (switch-player player)))
+              x (max v (min-r state alpha beta (inc depth) (switch-player curr-player) player))
               newalpha (max a-val x)
               ]
           (if (or (<= beta newalpha) (= size (- current 1)))
             (do
-              (println "pruned")
+              ;;(println "pruned")
               x)
             (recur x newalpha (inc current))
             )
@@ -147,21 +174,43 @@
 
     )
   )
+(defn find-move [utilityval state player]
+  (let [moves (get-valid-moves state player)]
+    (loop [ind 0]
+      (let [currentVal (utility (get moves ind) player)]
+        (if (not= utilityval currentVal)
+          (recur (inc ind))
+          (do
+            (binding [*out* *err*]
+              ;;(println (get moves ind))
+              )
+            ind
+            )
+          )
+        )
+      )
+    )
+  )
 (defn -main
   [& args]
   (loop []
+    (flush)
+    (let [gamestring (receive-game-string)]
+      (if-not (nil? gamestring)
+        (let [board (get gamestring "grid") width (get gamestring "width") height (get gamestring "height") player (get gamestring "player")]
+          (let [string (generate-string {:move 3})]
+            (binding [*out* *err*]
+              ;;(println string)
+              )
+            (println string)
+            )
+          )
+        )
+      )
     ;; Bind 'gamestring' to the string parsed from input.
-    (let [gamestring (parse-string (read-line))]
-      ;; Bind 'board', 'width', and 'height' to their correseponding values in the map we get from receive-game-string.
-      (let [board (get gamestring "grid") width (get gamestring "width") height (get gamestring "height") player (get gamestring "player")]
-        ;; Print each to validate.
-        (flush)
-        (println (generate-string {:move (random-move board player)}))
-        (flush)
-        ))
+
     (recur)
     )
-
   )
 
 
